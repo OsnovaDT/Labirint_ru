a49b748cbe9eaa051aa676b6cd96273a5428710d
Evgeny Stepanov <maestrojane@gmail.com>
2020-11-07 11:11:13 +0300

    Added models Author and PublishingHouse

--- books/models.py
+++ books/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        'Имя',
+        max_length=100,
+        unique=True,
+    )
+    info = models.TextField(
+        'Информация',
+        help_text='Информация об авторе',
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(
+        'Фотография',
+        null=True,
+        blank=True,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['name']
+
+
+class PublishingHouse(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=100,
+        unique=True,
+    )
+    info = models.TextField(
+        'Информация',
+        help_text='Информация об издательстве',
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        null=True,
+        blank=True,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Издательство'
+        verbose_name_plural = 'Издательства'
+        ordering = ['name']
