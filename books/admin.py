bd1eed8307bb8f657a3b45ff513f61a786da7106
Evgeny Stepanov <maestrojane@gmail.com>
2020-11-08 09:47:39 +0300

    Registered all models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import (
+    Book, Genre, Author,
+    PublishingHouse, Series
+)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'get_authors', 'publishing_house',
+        'series', 'price', 'get_genres'
+    )
+    list_display_links = (
+        'title', 'get_authors', 'publishing_house',
+        'series', 'get_genres'
+    )
+    search_fields = (
+        'title', 'get_authors', 'publishing_house',
+        'series', 'get_genres'
+    )
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'info', 'publishing_house',
+    )
+    list_display_links = (
+        'name', 'publishing_house',
+    )
+    search_fields = (
+        'name', 'publishing_house',
+    )
+
+
+class PublishingHouseAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'info',
+    )
+    list_display_links = (
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'info',
+    )
+    list_display_links = (
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(
+    PublishingHouse, PublishingHouseAdmin
+)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
