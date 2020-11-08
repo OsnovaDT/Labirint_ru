8bfc42c0339f2763e91c635e8952f81d94f1daf1
Evgeny Stepanov <maestrojane@gmail.com>
2020-11-08 11:08:38 +0300

    Added BookListView for index page

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from books.models import (
+    Book, Author, Genre,
+    PublishingHouse, Series
+)
+
+
+# For index page
+class BookListView(ListView):
+    template_name = 'books/index.html'
+    paginate_by = 12
+    context_object_name = 'books'
+    queryset = Book.objects.all()
